df3b313c129c4041bc7077cacf52c0110635faca
Rasika Liyanaarachchi <24977508+CodeBreakerRU@users.noreply.github.com>
2020-03-19 00:55:34 +0530

    Add files via upload

---
+++ SOAPAppClient/src/com/soapapp/service/PersonServiceImplProxy.java
@@ -0,0 +1,72 @@
+package com.soapapp.service;
+
+public class PersonServiceImplProxy implements com.soapapp.service.PersonServiceImpl {
+  private String _endpoint = null;
+  private com.soapapp.service.PersonServiceImpl personServiceImpl = null;
+  
+  public PersonServiceImplProxy() {
+    _initPersonServiceImplProxy();
+  }
+  
+  public PersonServiceImplProxy(String endpoint) {
+    _endpoint = endpoint;
+    _initPersonServiceImplProxy();
+  }
+  
+  private void _initPersonServiceImplProxy() {
+    try {
+      personServiceImpl = (new com.soapapp.service.PersonServiceImplServiceLocator()).getPersonServiceImpl();
+      if (personServiceImpl != null) {
+        if (_endpoint != null)
+          ((javax.xml.rpc.Stub)personServiceImpl)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+        else
+          _endpoint = (String)((javax.xml.rpc.Stub)personServiceImpl)._getProperty("javax.xml.rpc.service.endpoint.address");
+      }
+      
+    }
+    catch (javax.xml.rpc.ServiceException serviceException) {}
+  }
+  
+  public String getEndpoint() {
+    return _endpoint;
+  }
+  
+  public void setEndpoint(String endpoint) {
+    _endpoint = endpoint;
+    if (personServiceImpl != null)
+      ((javax.xml.rpc.Stub)personServiceImpl)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+    
+  }
+  
+  public com.soapapp.service.PersonServiceImpl getPersonServiceImpl() {
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl;
+  }
+  
+  public boolean deletePerson(int id) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.deletePerson(id);
+  }
+  
+  public com.soapapp.beans.Person[] getAllPersons() throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.getAllPersons();
+  }
+  
+  public com.soapapp.beans.Person getPerson(int id) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.getPerson(id);
+  }
+  
+  public boolean addPerson(com.soapapp.beans.Person p) throws java.rmi.RemoteException{
+    if (personServiceImpl == null)
+      _initPersonServiceImplProxy();
+    return personServiceImpl.addPerson(p);
+  }
+  
+  
+}
